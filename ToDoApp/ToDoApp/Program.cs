346c9d5abe880c7edad0bc5e225fad7b18a517b7
Lorant Lajos <trojanosall@gmail.com>
2017-09-28 11:19:43 +0200

    Week 04 - Day 04 - To Do App - Print usage done

---
+++ ToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome();
+            Console.ReadKey();
+
+        }
+
+        private static void Welcome()
+        {
+            Console.WriteLine("Command Line Todo application\n" + "=============================\n\n" + "Command line arguments:\n" + " -l   Lists all the tasks\n" + " -a   Adds a new task\n" + " -r   Removes an task\n" + " -c   Completes an task\n");
+
+        }
+    }
+}
